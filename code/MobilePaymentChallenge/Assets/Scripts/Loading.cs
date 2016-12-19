1c7f14e22a9e33c0416fab586e4391fc0d6196e1
Lucas Amorim <amorimlucas.nave@gmail.com>
2016-12-19 12:40:50 -0200

    Implement login behaviour using MundiPaggClientAPI

---
+++ code/MobilePaymentChallenge/Assets/Scripts/Loading.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MundiPagg.Internal;
+
+public class Loading : SingletonMonoBehaviour<Loading>
+{
+    [SerializeField]
+    private GameObject loadingObject;
+
+    void Start()
+    {
+        MundiPaggHTTP.OnRequestStart +=
+            () =>
+            {
+                loadingObject.SetActive(true);
+            };
+        MundiPaggHTTP.OnRequestEnd +=
+            () =>
+            {
+                loadingObject.SetActive(false);
+            };
+    }
+}
---
+++ code/MobilePaymentChallenge/Assets/Scripts/LoginManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using MundiPagg;
+using MundiPagg.MundiPaggClientModels;
+
+public class LoginManager : MonoBehaviour
+{
+    [SerializeField]
+    private InputField usernameField, passwordField;
+    
+    public void StartLogin()
+    {
+        string username = usernameField.text;
+        string password = passwordField.text;
+
+        LoginRequest request = new LoginRequest()
+        {
+            Username = username, Password = password
+        };
+
+        MundiPaggClientAPI.Login(request,
+            result =>
+            {
+                Debug.Log(result.Username);
+                Debug.Log(result.CustomerKey);
+            },
+            error =>
+            {
+                Debug.Log(error.ToString());
+            }
+        );
+    }
+}
